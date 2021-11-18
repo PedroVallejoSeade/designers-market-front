4905c8d316a848eb3d0551b1d1eeecd09cb39389
Isabela <i.ortiz@uniandes.edu.co>
2021-11-18 09:38:32 -0500

    cambios

--- src/components/designer-profile/DesignerProfileContent.js
+++ src/components/designer-profile/DesignerProfileContent.js
@@ -23,8 +23,30 @@ const DesignerProfileContent = () => {
                     <div ><a href="https://www.behance.net/iortiz495f" style={{ textDecoration: 'none' }} ><h3 className="designer-h3-2">Behance</h3></a></div>
                    
                 </div>
-                <div className="buiness-profile-content-column-2">
 
+                <div className="designer-profile-content-column-2">
+                    <div>
+                    <h2 className="designer-h3-3"> Áreas de interés y especialización</h2>
+                    </div>
+                    <div className="designer-profile-description">
+                        <div>
+                            <p className="texto-profile-description">Me interesa el diseño gráfico, en especial la creación de afiches para branding. Soy muy buena utilizando la suite de Adobe. Me gusta además la edición de videos publicitatios. </p>
+                        </div>
+                    </div>
+                    <div>
+                    <h2 className="designer-h3-3"> Calificación</h2>
+                    <div className="designer-profile-description-2">
+                        <div>
+                            <p className="texto-profile-description">Buena diseñadora, fue muy cumplida con los tiempos y receptiva a los cambios que le solicité. </p>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div className="designer-profile-description-2">
+                        <div>
+                            <p className="texto-profile-description">Tuvimos un contratiempo en el diseño porque no hubo comunicación clara de qué era lo que se quería, pero se logró resolver al final.</p>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
